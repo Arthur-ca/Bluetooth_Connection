e91505fcb0158174ef5b700535bb42a98b17c62f
Arthur-ca <ch243852@gmail.com>
2024-03-31 16:13:55 -0400

    Initial Commit

---
+++ bluetooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bluetooth
+//
+//  Created by Jiahao Chen on 2024-03-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bluetooth/bluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetoothApp.swift
+//  bluetooth
+//
+//  Created by Jiahao Chen on 2024-03-31.
+//
+
+import SwiftUI
+
+@main
+struct bluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
